6697a5138c941ec3893fd7e660c5a05f7f17918b
Greg Doud <gpdoud@gmail.com>
2020-06-17 14:11:42 -0400

    The main messages goes here
    * Added a blank to Program.cs
    * This is another detail

--- CsharpTypes/Program.cs
+++ CsharpTypes/Program.cs
@@ -2,15 +2,25 @@
 
 namespace CsharpTypes {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args) { 
 
             Console.Write("Enter first number : ");
             var nbr1AsString = Console.ReadLine();
             var nbr1 = Convert.ToInt32(nbr1AsString);
 
+            if(nbr1 < 100 || nbr1 > 990) {
+                    Console.WriteLine("Number must be <= 1000");
+                    return;
+            }
+
             Console.Write("Enter second number : ");
             var nbr2AsString = Console.ReadLine();
             var nbr2 = Convert.ToInt32(nbr2AsString);
+
+            if(nbr2 > 1000) {
+                Console.WriteLine("Number must be <= 1000");
+                return;
+            }
 
             var answer = nbr1 + nbr2;
 
